066812a6a6aa9731bcd3c592183604a3468d28ab
jkirlans5282 <Jacob@rgnt1-23-162-dhcp.int.colorado.edu>
2015-04-10 21:24:53 -0600

    1

---
+++ app.js
@@ -0,0 +1,16 @@
+Watson credential
+
+{
+  "personality_insights": [
+    {
+      "name": "Personality Insights-rh",
+      "label": "personality_insights",
+      "plan": "IBM Watson Personality Insights Monthly Plan",
+      "credentials": {
+        "url": "https://gateway.watsonplatform.net/personality-insights/api",
+        "username": "f14b9d0b-3ced-45c7-98c3-99ed0ec9e96f",
+        "password": "4ibDVHnNjoUW"
+      }
+    }
+  ]
+}
