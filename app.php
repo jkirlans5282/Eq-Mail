bee49dfd6f763859c99c832477cdb9938357c5b3
Prerit Oberai <preritoberai@yahoo.com>
2015-04-11 11:03:11 -0600

    2

--- app.php
+++ app.php
@@ -3,7 +3,7 @@
 
 define('CONSUMER_KEY', '6bbaozd7');
 define('CONSUMER_SECRET', 'WucIFMnI5UkHfruB');
-include_once("class.contextio.php");
+require_once '/class.contextio.php';
 
 function getContent() {
 
