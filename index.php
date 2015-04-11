431d6f49892278261c6e1aaf3dd15720fbfff769
Prerit Oberai <preritoberai@yahoo.com>
2015-04-11 14:16:58 -0600

    up

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 #!/usr/bin/php
+
 <?php
 // remove first line above if you're not running these examples through PHP CLI
 include_once("class.contextio.php");
@@ -50,33 +51,7 @@ foreach ($r->getData() as $message) {
 	
 fclose($fh);
 
-// EXAMPLE 3
-// Download all versions of the last 2 attachments exchanged with bill@widgets.com
-$saveToDir = dirname(__FILE__)."/".mt_rand(100,999);
-mkdir($saveToDir);
-$args = array('email'=>'bill@widgets.com', 'limit'=>2);
-echo "\nObtaining list of last two attachments exchanged with {$args['email']}\n";
-$r = $contextIO->listFiles($accountId, $args);
-foreach ($r->getData() as $document) {
-	echo "\nDownloading all versions of document \"".$document['file_name']."\"\n";
-	foreach ($document['occurrences'] as $attachment) {
-		echo "Downloading attachment '".$attachment['file_name']."' to $saveToDir ... ";
-		$contextIO->getFileContent($accountId, array('file_id'=>$attachment['fileId']), $saveToDir."/".$attachment['file_name']);
-		echo "done\n";
-	}
-}
-// EXAMPLE 4
-// Download all attachments with a file name that matches 'creenshot'
-$saveToDir = dirname(__FILE__)."/".mt_rand(100,999);
-mkdir($saveToDir);
-echo "\nDownloading all attachments matching 'creenshot'\n";
-$args = array('file_name'=>'creenshot');
-$r = $contextIO->listFiles($accountId, $args);
-foreach ($r->getData() as $attachment) {
-	echo "Downloading attachment '".$attachment['file_name']."' to $saveToDir ... ";
-	$contextIO->getFileContent($accountId, array('file_id'=>$attachment['file_id']), $saveToDir."/".$attachment['file_name']);
-	echo "done\n";
-}
+
 echo "\nall examples finished\n";
 
 $text="written into the Trans-Pacific Partnership (TPP) and other trade agreements being negotiated by the Office of the United States Trade Representative. I oppose “trade” policies that are developed without proper oversight or input from the public. The shear fact that wikileaks was the source to provide the full text of the bill should indicate that TPP outlines laws which are NOT in the best interests of the general public, since the laws had to be hidden from the public. TPP contains clauses which are unacceptable. These clauses will extend pharmaceutical drug patents, restrict internet freedoms, and create a legal framework for companies to sue nations over potential profit loss.
@@ -117,11 +92,11 @@ foreach($colors as &$color){
 		$color = "#FF0000";
 	}
 }
-echo("#one:{color:".$colors[0]);
-echo("#two:{color:".$colors[1]);
-echo("#three:{color:".$color[2]);
-echo("#four:{color:".$color[3]);
-echo("#five:{color:".$color[4]);
+//echo("#one:{color:".$colors[0]);
+//echo("#two:{color:".$colors[1]);
+//echo("#three:{color:".$color[2]);
+//echo("#four:{color:".$color[3]);
+//echo("#five:{color:".$color[4]);
 
 
 ?>
@@ -145,11 +120,19 @@ echo("#five:{color:".$color[4]);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <table style="width:100%">
-    <tr id="one"><td> Agreeableness </td><td> value </td></tr>
-    <tr id="two"><td> Conscientiousness </td><td> value </td></tr>
-    <tr id="three"><td> Extraversion </td><td> value </td></tr>
-    <tr id="four"><td> Emotional Range </td><td> value </td></tr>
-    <tr id="five"><td> Openness </td><td> value </td></tr>
+    <tr id="one"><td> item </td><td> value </td></tr>
+    <tr id="two"><td> item </td><td> value </td></tr>
+    <tr id="three"><td> item </td><td> value </td></tr>
+    <tr id="four"><td> item </td><td> value </td></tr>
+    <tr id="five"><td> item </td><td> value </td></tr>
+    <tr id="six"><td> item </td><td> value</td></tr>
     </table>
   </body>
+
+
+
+
+
+
+
 </HTML>
