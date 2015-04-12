50d084bc3841bf18e9ffc2f9aad2d4c90323a5b4
jkirlans5282 <Jacob@rgnt1-23-162-dhcp.int.colorado.edu>
2015-04-11 21:09:50 -0600

    c

--- index.php
+++ index.php
@@ -27,10 +27,10 @@ if (is_null($accountId)) {
 	die;
 }
 $toEmail='';
+// Get the most recent drafts message reciepient
 
 $toEmail= 'jacobkirlanstout@gmail.com';
-
-// Print the subject line of the last 100 emails sent FROM 
+// Print the subject line of the last 100 emails sent from with bill@widgets.com
 $args = array('from'=>$toEmail, 'limit'=>100);
 $r = $contextIO->listMessages($accountId, $args);
 foreach ($r->getData() as $message) {
@@ -38,18 +38,17 @@ foreach ($r->getData() as $message) {
 }
 
 // EXAMPLE 2
-// Print the Data  of the last 100 emails sent FROM 
+// Print the Data  of the last 100 emails sent from with bill@widgets.com
 
 $args = array('from'=>$toEmail, 'limit'=>100, 'include_body'=>1);
 //echo "\nGetting last 100 messages exchanged with {$args['from']}\n";
 $r = $contextIO->listMessages($accountId, $args);
-
+//echo($r);
 foreach ($r->getData() as $message) {
 	//echo "Message: " .$message['body'][0]['content'];
 	$text .= $message['body'][0]['content'];
 }
-
-
+	
 echo "\nall examples finished\n";
 
 $text = preg_replace("/[^A-Za-z0-9 ]/", '', $text);
@@ -82,25 +81,6 @@ foreach($traits as $trait){
 	echo($trait);
 }
 
-// Print the subject line of the last 100 emails sent TO contact
-$args2 = array('to'=>$toEmail, 'limit'=>100);
-$r2 = $contextIO->listMessages($accountId, $args2);
-foreach ($r2->getData() as $messageSent) {
-	echo "Subject[Reply]: ".$messageSent['subject']."\n";
-}
-
-// Print the Data  of the last 100 emails sent TO 
-
-$args2 = array('to'=>$toEmail, 'limit'=>100, 'include_body'=>1);
-//echo "\nGetting last 100 messages exchanged with {$args['to']}\n";
-$r2 = $contextIO->listMessages($accountId, $args2);
-foreach ($r2->getData() as $messageSent) {
-	echo "Message Sent Back: " .$messageSent['body'][0]['content'];
-	$textTo .= $messageSent['body'][0]['content'];
-}
-
-
-
 ?>
 
 <head>
@@ -119,7 +99,7 @@ foreach ($r2->getData() as $messageSent) {
 <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
 <style type="text/css">
 	#Self_Enhancement{
-		
+
 	}
 	#Excitement_Seeking{
 		background-color: <?=$lightGreen?>;
@@ -144,7 +124,10 @@ foreach ($r2->getData() as $messageSent) {
 <aside class="accordion">
 	<h1>Motivation</h1>
 	<div class="opened-for-codepen">
-		<h2 id = "Self_Enhancement">Self enhancement</h2>
+		<h2 id = "Self_Enhancement">
+			<span style = "float: left">Self Enhancement</span>
+			<span style = "float: right">$traits["Self_Enhancement"]</span>
+		</h2>
 		<p>Seeks personal success for themselves.</p>
 		
 		<h2 id ="Excitement_Seeking">Excitement Seeking</h2>
