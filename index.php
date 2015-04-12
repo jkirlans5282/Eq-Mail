ebe9a2b97085f5eb47d66636f958e0d7639d5ef3
jkirlans5282 <Jacob@rgnt1-23-162-dhcp.int.colorado.edu>
2015-04-11 18:55:14 -0600

    s

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ foreach ($r->getData() as $message) {
 	
 echo "\nall examples finished\n";
 */
-$text="Extending pharmaceutical patents beyond the current 20 the quick brown fox jumped over the lazy dog years will prevent generic drug production forcing patients to pay more for the same medication. This disincentives investment in research and development of new and better drugs, and at a time when drug companies already spend 1.5-2 times more on marketing, then on R&D, it is foolish to further desincentivize the advancement of life saving drugs. We need more effective drugs not more expensive ones. These high costs will also prevent the proliferation of these drugs in impoverished nations and locales where they are needed most."; //where text is the output of the context.io pull
+$text="Extending pharmaceuticaled without proper oversight or input from the public. The shear fact that wikileaks was the source to provide the full text of the bill should indicate that TPP outlines laws which are NOT in the best interests of the general public, since the laws had to be hidden from the public. TPP contains clauses which are unacceptable. These clauses will extend pharmaceutical ed without proper oversight or input from the public. The shear fact that wikileaks was the source to provide the full text of the bill should indicate that TPP outlines laws which are NOT in the best interests of the general public, since the laws had to be hidden from the public. TPP contains clauses which are unacceptable. These clauses will extend pharmaceutical patents beyond the current 20 the quick brown fox jumped over the lazy dog years will prevent generic drug production forcing patients to pay more for the same medication. This disincentives investment in research and development of new and better drugs, and at a time when drug companies already spend 1.5-2 times more on marketing, then on R&D, it is foolish to further desincentivize the advancement of life saving drugs. We need more effective drugs not more expensive ones. These high costs will also prevent the proliferation of these drugs in impoverished nations and locales where they are needed most."; //where text is the output of the context.io pull
 
 $watsonString= "$'".$text."'";
 $command = "curl 'https://gateway.watsonplatform.net/personality-insights/api/v2/profile?header=false' -H 'Authorization: Basic ZjE0YjlkMGItM2NlZC00NWM3LTk4YzMtOTllZDBlYzllOTZmOjRpYkRWSG5Oam9VVw==' -H 'Origin: chrome-extension://fdmmgilgnpjigdojojpjoooidkmcomcm' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36' -H 'Content-Language: en' -H 'Accept: application/json' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' -H 'Content-Type: text/plain' --data-binary ".$watsonString." --compressed";
@@ -71,8 +71,14 @@ $watsonOutput = exec($command);
 //echo("\n");
 //echo($location."\n");
 $watsonOutput = json_decode($watsonOutput, true);
-//echo($watsonOutput['tree']['children']['id']);
-echo("here");
+$traits = array(
+				"Self_Enhancement" => $watsonOutput['tree']['children'][2]['children'][0]['percentage'], //self-enhancement
+				"Excitement_Seeking" => $watsonOutput['tree']['children'][2]['children'][3]['percentage'], //excitement seeking
+				"Challenge" => $watsonOutput['tree']['children'][1]['children'][0]['children'][0]['percentage'],
+				
+				);
+
+echo($traits["Self_Enhancement"]);
 
 ?>
 
@@ -88,8 +94,8 @@ echo("here");
 
 <body>
 
-  <link href='http://fonts.googleapis.com/css?family=News+Cycle:400,700' rel='stylesheet' type='text/css'>
-<link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=News+Cycle:400,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
 <style type="text/css">
 
 	#Excitement_Seeking{
@@ -115,7 +121,7 @@ echo("here");
 <aside class="accordion">
 	<h1>Motivation</h1>
 	<div class="opened-for-codepen">
-		<h2>Self enhancement</h2>
+		<h2 id = "Self_Enhancement">Self enhancement</h2>
 		<p>Seeks personal success for themselves.</p>
 		
 		<h2 id ="Excitement_Seeking">Excitement Seeking</h2>
