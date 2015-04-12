434889ca122d06596f8fed5d11e2b25c11500a18
jkirlans5282 <Jacob@rgnt1-23-162-dhcp.int.colorado.edu>
2015-04-12 03:21:28 -0600

    Fucking awesome

--- chromeExtension/app.js
+++ chromeExtension/app.js
@@ -1,4 +1,27 @@
 //<div>Icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
-var email="jacobkirlanstout@gmail.com";
+
+var email="blank";
 document.getElementById('iframe').src="https://csel.cs.colorado.edu/~jaki2391/index.php?email="+email;
 document.getElementById('name').innerHTML+=email
+
+
+chrome.tabs.query({
+    active: true,               // Select active tabs
+    lastFocusedWindow: true     // In the current window
+}, function(array_of_Tabs) {
+    // Since there can only be one active tab in one active window, 
+    //  the array has only one element
+    chrome.tabs.executeScript(array_of_Tabs[0].id,{code:'document.getElementsByName("to")[0].defaultValue;'},function(returnArray){email=returnArray[0]; setIframe(email); }
+);
+    // Example:
+});
+function setIframe(email){
+	console.log(email);
+	email = email.substr(email.search("<")+1);
+    if(email.substr(length-1)==">"){
+	   email=email.substr(0, email.length-1);
+    }
+	document.getElementById('iframe').src="https://csel.cs.colorado.edu/~jaki2391/index.php?email="+email;
+	document.getElementById('name').innerHTML="PERSONAL EMAIL FOR: "+email;
+
+}
