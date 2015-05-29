b2aeedd07621a1f6afa273f6f35d0e19530286ec
jkirlans5282 <jacobkirlanstout@gmail.com>
2015-05-29 17:17:47 -0600

    fixed syntax bugs in cookie.php

--- backend/cookie.php
+++ backend/cookie.php
@@ -7,7 +7,7 @@ function checkForCookie(){
 	if(!isset($_COOKIE[$cookie_name])){
 		try{
 			setcookie($cookie_name, $cookie_value, 2147483647); //google 2038 bug for more info. essentiallly max value. 
-		}catch Exception $e{
+		}catch(Exception $e){
 			fwrite($logFile,"cookies disabled, or something like that, line 9 cookie.php");
 		}
 	}
@@ -17,10 +17,10 @@ function addProfile($email, $profileArray){
 	if(!getProfile($email))
 	{
 		$tempArray = array($email=>$profileArray); // makes a multidemensional array where email points to profile dictionary
-		$cookie_value = unserialize($_COOKIE[$cookie_name]) //returns dictionary.
+		$cookie_value = unserialize($_COOKIE[$cookie_name]); //returns dictionary.
 		$cookie_value = array_merge($tempArray, $cookie_value);	//merge arrays
 		$cookie_value = serialize($cookie_value); // serialize array into string for cookie set. 
-		setcookie($cookie_name, $cookie_value, 2147483647)
+		setcookie($cookie_name, $cookie_value, 2147483647);
 		$_COOKIE[$cookie_name];
 	}else{
 		fwrite("user already exists");
@@ -39,7 +39,7 @@ function addProfile($email, $profileArray){
 //returns null if profile does not exist.
 function getProfile($email){
 	if(isset($_COOKIE[$cookie_name])){
-		$profile =unserialize($_COOKIE[$cookie_name]) //I think this will work, if theres something wrong with the return value or it never returns anything its probably this line -Jacob
+		$profile =unserialize($_COOKIE[$cookie_name]); //I think this will work, if theres something wrong with the return value or it never returns anything its probably this line -Jacob
 		if($profile){
 			return $profile[$email];
 		}else{
@@ -50,5 +50,4 @@ function getProfile($email){
 		fwrite($logFile, "Unable to find cookie line 44 cookie.php");
 	}
 }
-$_COOKIE();
 ?>
